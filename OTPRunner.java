b20ac0c005dbd7418b8aa5e8294cc7734811746a
eturn8 <100147455@ccisd.net>
2025-09-08 08:18:16 -0500

    Create OTPRunner.java

---
+++ OTPRunner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class OTPRunner {
+    public static void main(String args[])
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Username/ Email");
+        String username= input.nextLine();
+        System.out.println(username);
+
+    }
+}
