f6bb7bafe5542b423e2c8a25aa59484278dcfd71
eturn8 <100147455@ccisd.net>
2025-09-08 08:17:29 -0500

    Create OTP.java

---
+++ OTP.java
@@ -0,0 +1,30 @@
+//import java.util.*;
+//public class OTP {
+//    String password;
+//    String email;
+//    public OTP(String userEmail)
+//    {
+//        email=userEmail;
+//    }
+//    public boolean checkPass(String input)
+//    {
+//        if(password.equals(input))
+//        {
+//            return true;
+//        }
+//        else
+//        {
+//            System.out.println("Would you like to resend code?");
+//            return false;
+//        }
+//    }
+//    public createPass()
+//    {
+//
+//    }
+//    public passExpire()
+//    {
+//
+//    }
+//
+//}
