21aa2f0d93b9f3b0f821cb53a7c85a314ea6b2a7
eturn8 <100147455@ccisd.net>
2025-09-22 21:15:01 -0500

    Update OTP.java

--- OTP.java
+++ OTP.java
@@ -1,30 +1,30 @@
-//import java.util.*;
-//public class OTP {
-//    String password;
-//    String email;
-//    public OTP(String userEmail)
-//    {
-//        email=userEmail;
-//    }
-//    public boolean checkPass(String input)
-//    {
-//        if(password.equals(input))
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            System.out.println("Would you like to resend code?");
-//            return false;
-//        }
-//    }
-//    public createPass()
-//    {
-//
-//    }
-//    public passExpire()
-//    {
-//
-//    }
-//
-//}
+import java.util.*;
+public class OTP {
+    String password;
+    String email;
+    public OTP(String userEmail)
+    {
+        email=userEmail;
+    }
+    public boolean checkPass(String input)
+    {
+        if(password.equals(input))
+        {
+            return true;
+        }
+        else
+        {
+            System.out.println("Would you like to resend code?");
+            return false;
+        }
+    }
+    public String createOTP()
+    {
+        String oneTimeCode="";
+        for(int i=0;i<6;i++)
+        {
+           oneTimeCode+= Integer.toString((int)(Math.random()*10));
+        }
+        return oneTimeCode;
+    }
+}
